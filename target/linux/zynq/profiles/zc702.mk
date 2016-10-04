openwrt/openwrt

commit 9945a1dca5bb6bc522393f7583baf3a64df6ce11

build: remove cpio.gz and tar.gz from regular filesystem types
They are not used to produce regular firmware images anyway. Instead,
call their build templates directly if enabled in the config

Signed-off-by: Felix Fietkau <nbd@nbd.name>

target/linux/zynq/image/Makefile
@@ -22,9 +22,6 @@ define Image/boot-imgs
 	$(call Image/BuildDTB,$(DTS_DIR)/$(DEVICE_DTS).dts,\
 		$(BIN_DIR)/$(IMG_PREFIX)-system.dtb)
 
-	$(call Image/mkfs/cpiogz)
-	$(call Image/mkfs/targz)
-
 	# Create uboot cpio.gz
 	mkimage -A arm -T ramdisk -C gzip -n "$(PROFILE) OpenWRT rootfs" \
 		-d $(BIN_DIR)/$(IMG_PREFIX)-rootfs.cpio.gz \

include/image.mk
@@ -78,8 +78,6 @@ fs-types-$(CONFIG_TARGET_ROOTFS_EXT4FS) += ext4
 fs-types-$(CONFIG_TARGET_ROOTFS_ISO) += iso
 fs-types-$(CONFIG_TARGET_ROOTFS_UBIFS) += ubifs
 fs-subtypes-$(CONFIG_TARGET_ROOTFS_JFFS2) += $(addsuffix -raw,$(addprefix jffs2-,$(JFFS2_BLOCKSIZE)))
-fs-subtypes-$(CONFIG_TARGET_ROOTFS_CPIOGZ) += cpiogz
-fs-subtypes-$(CONFIG_TARGET_ROOTFS_TARGZ) += targz
 
 TARGET_FILESYSTEMS := $(fs-types-y)
 
@@ -217,16 +215,6 @@ define Image/mkfs/ubifs
 		-o $@ -d $(TARGET_DIR)
 endef
 
-define Image/mkfs/cpiogz
-	( cd $(TARGET_DIR); find . | cpio -o -H newc | gzip -9n >$(BIN_DIR)/$(IMG_PREFIX)-rootfs.cpio.gz )
-endef
-
-define Image/mkfs/targz
-	$(TAR) -cp --numeric-owner --owner=0 --group=0 --sort=name \
-		$(if $(SOURCE_DATE_EPOCH),--mtime="@$(SOURCE_DATE_EPOCH)") \
-		-C $(TARGET_DIR)/ . | gzip -9n > $(BIN_DIR)/$(IMG_PREFIX)$(if $(PROFILE_SANITIZED),-$(PROFILE_SANITIZED))-rootfs.tar.gz
-endef
-
 E2SIZE=$(shell echo $$(($(CONFIG_TARGET_ROOTFS_PARTSIZE)*1024*1024)))
 
 define Image/mkfs/ext4
@@ -258,6 +246,20 @@ define Image/Checksum
 		$(FIND) -maxdepth 1 -type f \! -name 'md5sums'  -printf "%P\n" | sort | xargs $1 > $2 \
 	)
 endef
+
+ifdef CONFIG_TARGET_ROOTFS_TARGZ
+  define Image/Build/targz
+	$(TAR) -cp --numeric-owner --owner=0 --group=0 --sort=name \
+		$(if $(SOURCE_DATE_EPOCH),--mtime="@$(SOURCE_DATE_EPOCH)") \
+		-C $(TARGET_DIR)/ . | gzip -9n > $(BIN_DIR)/$(IMG_PREFIX)$(if $(PROFILE_SANITIZED),-$(PROFILE_SANITIZED))-rootfs.tar.gz
+  endef
+endif
+
+ifdef CONFIG_TARGET_ROOTFS_CPIOGZ
+  define Image/Build/cpiogz
+	( cd $(TARGET_DIR); find . | cpio -o -H newc | gzip -9n >$(BIN_DIR)/$(IMG_PREFIX)-rootfs.cpio.gz )
+  endef
+endif
 
 $(KDIR)/root.%: kernel_prepare
 	$(call Image/mkfs/$(word 1,$(subst +,$(space),$*)),$(subst +,$(space),$*))
@@ -478,6 +480,8 @@ define BuildImage
 	$(call Image/mkfs/prepare)
 
   kernel_prepare: mkfs_prepare
+	$(call Image/Build/targz)
+	$(call Image/Build/cpiogz)
 	$(call Image/BuildKernel)
 	$(if $(CONFIG_TARGET_ROOTFS_INITRAMFS),$(if $(IB),,$(call Image/BuildKernel/Initramfs)))
 	$(call Image/InstallKernel)

stang6/nxp-qoriq-openwrt

commit 664a70c242c8f505311e6d6ac370212147a6a6f5

zynq: convert to new image build code
Convert the Zynq target to use the new image build code in order to fix
broken image generation after 9945a1dca5bb6bc522393f7583baf3a64df6ce11
changed the handling of CPIO images.

Also remove the misapplied ubifs feature flag since the image generation is
not using UBIFS for building FIT images.

As part of the conversion, move the DTB building and the uImage ramdisk
generation into separate build steps which can be generalized and shared
with other targets, like APM821xx, in the long run.

Also remove the legacy profile declarations and use new-style per-device
profiles which will allow different package sets per image in the future.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

target/linux/zynq/Makefile
@@ -10,7 +10,7 @@ include $(INCLUDE_DIR)/host.mk
 ARCH:=arm
 BOARD:=zynq
 BOARDNAME:=Xilinx Zynq 7000 SoCs
-FEATURES:=fpu gpio rtc usb usbgadget targz ubifs
+FEATURES:=fpu gpio rtc usb usbgadget targz
 CPU_TYPE:=cortex-a9
 CPU_SUBTYPE:=neon
 MAINTAINER:=Jason Wu <jason.wu.misc@gamil.com>

target/linux/zynq/image/Makefile
@@ -11,33 +11,32 @@ include $(INCLUDE_DIR)/image.mk
 # Images
 #################################################
 
-define Image/boot-imgs
-	# Copy zImage to BIN_DIR
-	$(CP) $(KDIR)/zImage $(BIN_DIR)/$(IMG_PREFIX)-zImage
+# $(1): rootfs contents directory
+define Image/mkfs/cpio
+	( cd $(call mkfs_target_dir,$(1))/; find . | cpio -o -H newc | gzip -9n > $@ )
+endef
 
-	$(call Image/BuildKernel/MkuImage, none, $(KERNEL_LOADADDR), \
-		$(KERNEL_ENTRY_POINT), $(KDIR)/zImage, \
-		$(BIN_DIR)/$(IMG_PREFIX)-uImage)
+# $(1): zynq board id
+define Build/dtb
+	$(call Image/BuildDTB,$(DTS_DIR)/zynq-$(1).dts,$@.dtb)
+endef
 
-	$(call Image/BuildDTB,$(DTS_DIR)/$(DEVICE_DTS).dts,\
-		$(BIN_DIR)/$(IMG_PREFIX)-system.dtb)
-
+define Build/uRamdisk
 	# Create uboot cpio.gz
 	mkimage -A arm -T ramdisk -C gzip -n "$(PROFILE) OpenWRT rootfs" \
-		-d $(BIN_DIR)/$(IMG_PREFIX)-rootfs.cpio.gz \
-		$(BIN_DIR)/$(IMG_PREFIX)-uramdisk.image.gz
+		-d $(IMAGE_ROOTFS) $@.new
+	mv $@.new $@
+endef
 
+# $(1): FIT name/description
+define Build/fit
 	# create FIT image with rootfs
 	./mkits.sh \
-		-D $(DEVICE_LC) -o $(KDIR)/fit-$(DEVICE_LC).its -k $(KDIR)/zImage \
-		-d $(BIN_DIR)/$(IMG_PREFIX)-system.dtb \
-		-C none -a $(KERNEL_LOADADDR) -e $(KERNEL_ENTRY_POINT) \
-		-A $(ARCH) -v $(LINUX_VERSION) \
-		-r $(BIN_DIR)/$(IMG_PREFIX)-rootfs.cpio.gz -z gzip
-	PATH=$(LINUX_DIR)/scripts/dtc:$(PATH) mkimage -f $(KDIR)/fit-$(DEVICE_LC).its $(KDIR)/fit-$(DEVICE_LC).itb
-	$(CP) $(KDIR)/fit-$(DEVICE_LC).itb $(BIN_DIR)/$(IMG_PREFIX)-fit.itb
-	ln -fs $(IMG_PREFIX)-fit.itb $(BIN_DIR)/fit.itb
+		-D $(1) -o $@.its -k $(IMAGE_KERNEL) -d $(IMAGE_KERNEL).dtb \
+		-C none -a $(KERNEL_LOADADDR) -e $(KERNEL_ENTRY) \
+		-A $(ARCH) -v $(LINUX_VERSION) -r $(IMAGE_ROOTFS) -z gzip
 
+	PATH=$(LINUX_DIR)/scripts/dtc:$(PATH) mkimage -f $@.its $@
 endef
 
 #################################################
@@ -46,36 +45,27 @@ endef
 
 # default kernel load address
 KERNEL_LOADADDR=0x8000
-KERNEL_ENTRY_POINT=0x8000
+KERNEL_ENTRY=0x8000
 
-### Device macros ###
-define Device/Default
-	IMG_PREFIX := zynq
-	PROFILE_SANITIZED :=
+TARGET_FILESYSTEMS += cpio
+
+# $(1): canonical device name
+# $(2): lowercase device id
+define ZynqDevice
+  define Device/$(1)
+	DEVICE_TITLE := $(1) development board
+	KERNEL := dtb $(2) | kernel-bin | uImage none
+	KERNEL_NAME = zImage
+	IMAGES := uramdisk.image.gz fit.itb
+	IMAGE/uramdisk.image.gz := uRamdisk
+	IMAGE/fit.itb := fit $(2)
+	IMAGE_NAME = $$$$(IMG_PREFIX)-$(2)-$$$$(2)
+  endef
+  TARGET_DEVICES += $(1)
 endef
 
-define Device/DefaultConfig
-	DEVICE_LC = $(1)
-	IMG_PREFIX = zynq-$(1)
-	DEVICE_DTS = zynq-$(1)
-endef
-
-### ZC702 ###
-define Device/ZC702
-	$(call Device/DefaultConfig,zc702)
-endef
-
-define Device/ZED
-	$(call Device/DefaultConfig,zed)
-endef
-
-define Device/ZYBO
-	$(call Device/DefaultConfig,zybo)
-endef
-
-define Image/BuildKernel
-	$(eval $(call Device/$(PROFILE)))
-	$(call Image/boot-imgs)
-endef
+$(eval $(call ZynqDevice,ZC702,zc702))
+$(eval $(call ZynqDevice,ZedBoard,zed))
+$(eval $(call ZynqDevice,ZYBO,zybo))
 
 $(eval $(call BuildImage))

target/linux/zynq/profiles/zc702.mk
@@ -1,16 +1,0 @@
-#
-# Copyright (C) 2015 OpenWrt.org
-#
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
-#
-
-define Profile/ZC702
-	NAME:=ZC702 development baord
-endef
-
-define Profile/ZC702/Description
-	Build firmware image for Xilinx Zynq ZC702 development board.
-endef
-
-$(eval $(call Profile,ZC702))

target/linux/zynq/profiles/zed.mk
@@ -1,16 +1,0 @@
-#
-# Copyright (C) 2015 OpenWrt.org
-#
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
-#
-
-define Profile/ZED
-	NAME:=ZedBoard development baord
-endef
-
-define Profile/ZED/Description
-	Build firmware image for Avnet Digilent ZedBoard development board.
-endef
-
-$(eval $(call Profile,ZED))

target/linux/zynq/profiles/zybo.mk
@@ -1,16 +1,0 @@
-#
-# Copyright (C) 2016 OpenWrt.org
-#
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
-#
-
-define Profile/ZYBO
-	NAME:=ZYBO development baord
-endef
-
-define Profile/ZYBO/Description
-	Build firmware image for Digilent ZYBO development board.
-endef
-
-$(eval $(call Profile,ZYBO))