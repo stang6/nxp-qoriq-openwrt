openwrt/openwrt

commit aa6f5f1787a69c8b9af7533010f2d099075d9826

kirkwood: use image metadata
Append and enforce image metadata. Remove the device specific image
checks, they are replaced by image metadata.

Signed-off-by: Mathias Kresin <dev@kresin.me>

target/linux/kirkwood/base-files/lib/upgrade/platform.sh
@@ -1,27 +1,9 @@
 RAMFS_COPY_BIN='fw_printenv fw_setenv'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
+REQUIRE_IMAGE_METADATA=1
+
 platform_check_image() {
-	[ "$#" -gt 1 ] && return 1
-	local board="$(board_name)"
-	local magic="$(get_magic_long "$1")"
-
-	case "$board" in
-	"linksys-audi"|\
-	"linksys-viper")
-		[ "$magic" != "27051956" -a "$magic" != "73797375" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	*)
-		nand_do_platform_check $board $1
-		return $?
-		;;
-	esac
-
-	echo "Sysupgrade is not yet supported on $board."
 	return 1
 }
 

target/linux/kirkwood/image/Makefile
@@ -22,8 +22,9 @@ define Device/Default
   SUBPAGESIZE := 512
   BLOCKSIZE := 128k
   IMAGES := sysupgrade.bin factory.bin
-  IMAGE/sysupgrade.bin := sysupgrade-tar
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   IMAGE/factory.bin := append-ubi
+  SUPPORTED_DEVICES := $(1)
 endef
 
 define Device/dockstar

stang6/nxp-qoriq-openwrt

commit 3f2f6112f3bd053704d1bcc1eb5728678cf30f4e

kirkwood: fix sysupgrade
The platform_check_image() stub need to return 0 for success, otherwise
the sysupgrade will fail with:

  Image check 'platform_check_image' failed.

Fixes: aa6f5f1787a6 ("kirkwood: use image metadata")
Signed-off-by: 尤晓杰 <yxj790222@163.com>
[reworded commit message}
Signed-off-by: Mathias Kresin <dev@kresin.me>

target/linux/kirkwood/base-files/lib/upgrade/platform.sh
@@ -4,7 +4,7 @@ RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 REQUIRE_IMAGE_METADATA=1
 
 platform_check_image() {
-	return 1
+	return 0
 }
 
 platform_do_upgrade() {