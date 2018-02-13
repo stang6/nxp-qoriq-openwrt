lede-project/source

ramips: add support for D-Link DAP-1522 A1
D-Link DAP-1522 is a wireless bridge/access point with 4 LAN
ports and a dual-band wireless chipset.

Specifications:
- Ralink RT2880
- 32 MB of RAM
- 4 MB of Flash
- 4x 10/100/1000 Mbps Ethernet (RTL8366SR)
- 802.11abgn (RT2850)

Flash Instructions:
1. Download lede-ramips-rt288x-dap-1522-a1-squashfs-factory.bin
2. Open the web interface and upload the image

Signed-off-by: George Hopkins <george-hopkins@null.net>

target/linux/ramips/base-files/etc/diag.sh
@@ -138,6 +138,12 @@ get_status_led() {
 	youku-yk1)
 		status_led="$board:blue:power"
 		;;
+	dap-1522-a1|\
+	k2p|\
+	m3|\
+	miwifi-nano)
+		status_led="$board:blue:status"
+		;;
 	db-wrt01|\
 	esr-9753|\
 	pbr-d1)
@@ -163,11 +169,6 @@ get_status_led() {
 		;;
 	hc5962)
 		status_led="$board:white:status"
-		;;
-	k2p|\
-	m3|\
-	miwifi-nano)
-		status_led="$board:blue:status"
 		;;
 	linkits7688| \
 	linkits7688d)

stang6/nxp-qoriq-openwrt

target/linux/ramips/base-files/etc/diag.sh
@@ -147,6 +147,13 @@ get_status_led() {
 	youku-yk1)
 		status_led="$boardname:blue:power"
 		;;
+	dap-1522-a1|\
+	k2p|\
+	m3|\
+	mir3g|\
+	miwifi-nano)
+		status_led="$boardname:blue:status"
+		;;
 	db-wrt01|\
 	esr-9753|\
 	pbr-d1)
@@ -173,12 +180,6 @@ get_status_led() {
 		;;
 	hc5962)
 		status_led="$boardname:white:status"
-		;;
-	k2p|\
-	m3|\
-	mir3g|\
-	miwifi-nano)
-		status_led="$boardname:blue:status"
 		;;
 	linkits7688)
 		status_led="linkit-smart-7688:orange:wifi"